6699082227481c8f1fe269dc6e8d1a75b9305d31
malikibrarbhutta@gmail.com <malikibrarbhutta@gmail.com>
2024-09-08 19:38:05 +0500

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ export const Contact = () => {
         };
 
         try {
-            const response = await fetch('https://malikibrar.vercel.app/submit/', {
+            const response = await fetch('https://malik-ibrar-developer.vercel.app/submit/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
